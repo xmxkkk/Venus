df13578e580eba7c10952451fd4846cb3ee4f703
xmxkkk <908581016@qq.com>
2016-11-30 10:26:19 +0800

    d

--- package.bat
+++
@@ -1,5 +0,0 @@
-cd .
-
-mvn package
-
-pause
--- run.bat
+++
@@ -1,5 +0,0 @@
-cd .
-
-java -jar target/venus-0.0.1-SNAPSHOT.jar --command.sct=start --command.sct.cache=false --command.sct.force=false --command.dt=start --command.lst=start --command.ct=start --command.shutdown=none
-
-pause
