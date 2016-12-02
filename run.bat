d3ebfa7a28c5d9cb0ea325cd4b1d6c172e86a62b
xmxkkk <908581016@qq.com>
2016-12-02 20:15:49 +0800

    aa

---
+++ run.bat
@@ -0,0 +1,5 @@
+cd .
+
+java -jar ./target/venus-0.0.1-SNAPSHOT.jar --command.sct=start --command.sct.cache=false --command.sct.force=true --command.dt=start --command.lst=start --command.lst.id=0  --command.ct=none --command.shutdown=none
+
+pause
